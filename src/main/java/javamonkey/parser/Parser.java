ef11ad9d04826e23bfc483415bb06cc33b21efab
Jonathan Ly <lyjonathan02@gmail.com>
2024-08-01 00:57:44 -0700

    Basic parsing structure

--- src/main/java/javamonkey/parser/Parser.java
+++ src/main/java/javamonkey/parser/Parser.java
@@ -1,6 +1,9 @@
 package javamonkey.parser;
 
+import java.util.ArrayList;
+
 import javamonkey.ast.Program;
+import javamonkey.ast.Statement;
 import javamonkey.lexer.Lexer;
 import javamonkey.token.Token;
 
@@ -20,8 +23,38 @@ public class Parser {
         this.peekToken = this.l.nextToken();
     }
 
+    /**
+     * Parse an the input string defined by the lexer in this object.
+     * 
+     * @return an AST root Program, the parsed program
+     */
     public Program parse() {
         Program p = new Program();
+        p.statements = new ArrayList<>();
+
+        while (!this.curToken.type.equals(Token.EOF)) {
+            Statement stmt = this.parseStatement();
+            if (stmt != null) {
+                p.statements.add(stmt);
+            }
+
+            this.nextToken();
+        }
+
+        return p;
+    }
+
+    // Helper method to parse statements.
+    private Statement parseStatement() {
+        return switch (this.curToken.type) {
+            case Token.LET -> this.parseLetStatement();
+            default -> null;
+        };
+    }
+
+    // Helper method to parse let statements.
+    private Statement parseLetStatement() {
+        // TODO: implement this method (bottom p. 40)
         return null;
     }
 }
