4834e2cbd2187df5d5529e2aaf89e4769d0c1417
Jonathan Ly <lyjonathan02@gmail.com>
2024-08-03 01:21:41 -0700

    Add tests for AST

---
+++ src/test/java/TestAST.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import javamonkey.ast.Identifier;
+import javamonkey.ast.LetStatement;
+import javamonkey.ast.Program;
+import javamonkey.token.Token;
+
+public class TestAST {
+    @Test
+    public void testString() {
+        Program prog = new Program();
+
+        // AST for `let myVar = anotherVar;`
+        prog.statements = List.of(
+            new LetStatement(
+                new Token(Token.LET, "let"), 
+                new Identifier(
+                    new Token(Token.IDENT, "myVar"), 
+                    "myVar"
+                ), 
+                new Identifier(
+                    new Token(Token.IDENT, "anotherVar"),
+                    "anotherVar"
+                )
+            )
+        );
+
+        assertEquals("let myVar = anotherVar;", prog.toString());
+    }
+}
